bb49fa2af1b124f1daee4a2ee95533fa5deda5cd
hob71 <roberthobday@sky.com>
2020-10-04 09:54:38 +0000

    Added additional pages to nav bar

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/solvents')
+def solvents():
+    return render_template("solvents.html")
+    
+
+@app.route('/consumables')
+def consumables():
+    return render_template("consumables.html")
+
+
+@app.route('/request')
+def request():
+    return render_template("request.html")
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
